121244288c599d879450c6bc69560c6e828caed7
Pozsa Marcell <pozsmarc112@gmail.com>
2023-02-23 19:03:57 +0100

    Equalizer

--- src/metódusgyak_pozsa_marcell/Metódusgyak_pozsa_marcell.java
+++ src/metódusgyak_pozsa_marcell/Metódusgyak_pozsa_marcell.java
@@ -12,11 +12,26 @@ public class Metódusgyak_pozsa_marcell {
     public static void main(String[] args){
         int osszeg= elso10szamOsszege();
         String kimenet="Az első 10 szám összege: " +osszeg +"\n";
-        kiir(kimenet);
+       //kiir(kimenet);
         
         int szam1=7, szam2 =4 ;
-        kiir("%d + %d =%d\n".formatted(szam1,szam2,(szam1+szam2)));
-        kiir("°%d +%d+%d+%d=%d\n".formatted(szam1 ,szam2,osszeg,(szam1+szam2),(szam1+szam2+osszeg+(szam1+szam2))));
+      //  kiir("%d + %d =%d\n".formatted(szam1,szam2,(szam1+szam2)));
+       // kiir("%d +%d+%d+%d=%d\n".formatted(szam1 ,szam2,osszeg,(szam1+szam2),(szam1+szam2+osszeg+(szam1+szam2))));
+       osszeg=osszead(1,2);
+       osszeg=osszead(osszeg,3);
+       osszeg=osszead(osszeg,3);
+       osszeg=osszead(osszeg,4);
+       kiir("1+2+3+4= "+osszeg+"\n");
+       
+       int a=3,b=4,c=2;
+       osszeg=osszead(a,b);
+       osszeg=osszead(osszeg,c);
+       String s = "%d+%d+%d gyöke:".formatted(a,b,c);
+       double gyok = Math.sqrt(osszeg);
+       kimenet = String.format("%s %.4f\n",s,gyok);
+       kiir(kimenet);
+       
+       
     }
     private static int elso10szamOsszege() {
         //osszead
@@ -28,8 +43,8 @@ public class Metódusgyak_pozsa_marcell {
         //System.out.println("Az első 10 szám összege: "+ osszeg);
         return osszeg;
     }
-    private static int osszead(int a ,int b){
-        //osszead és kiír
+    private static int osszead(int a,int b ){
+         //osszead és kiír
         //System.out.printf("°%d + %d = %d\n",a,b,a+b);
         return a+b;
     }
